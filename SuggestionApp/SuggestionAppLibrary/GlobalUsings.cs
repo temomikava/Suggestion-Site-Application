90cc3a1469a0f5d4269ba77b3686f075bdf32806
temomikava <temsonmikava@gmail.com>
2022-04-23 19:28:24 +0400

    created  models

--- SuggestionApp/SuggestionAppLibrary/GlobalUsings.cs
+++ SuggestionApp/SuggestionAppLibrary/GlobalUsings.cs
@@ -1,2 +1,4 @@
 ﻿global using SuggestionAppLibrary.Models;
 global using SuggestionAppLibrary.DataContext;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
---
+++ SuggestionApp/SuggestionAppLibrary/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuggestionAppLibrary.Models;
+public class UserModel
+{
+   [BsonId]
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string ObjectIdentifier { get; set; }
+   public string FirstName { get; set; }
+   public string LastName { get; set; }
+   public string DisplayName { get; set; }
+   public string EmailAddress { get; set; }
+   public List<BasicSuggestionModel> AutoredSuggestions { get; set; } = new();
+   public List<BasicSuggestionModel> VotedOnSuggestions { get; set; } = new();
+}
